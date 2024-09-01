18b521d14c58e2261669e341dde305115128f26b
Sebastian <Sebastiandd87@gmail.com>
2024-08-31 23:04:28 -0500

    add function post in server

--- Index.js
+++ Index.js
@@ -2,12 +2,41 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-// Ruta de ejemplo
+
 app.get('/', (req, res) => {
   res.send('¡Hola, Express!');
 });
 
-// Iniciar servidor
+app.post('/save-data', (req, res) => {
+  let body = '';
+
+  req.on('data', chunk => {
+    body += chunk.toString();
+  });
+
+  req.on('end', () => {
+    try {
+      const data = JSON.parse(body);
+      
+      const filePath = path.join(__dirname, 'data.json');
+      
+      let existingData = [];
+      if (fs.existsSync(filePath)) {
+        const fileContent = fs.readFileSync(filePath);
+        existingData = JSON.parse(fileContent);
+      }
+
+      existingData.push(data);
+      
+      fs.writeFileSync(filePath, JSON.stringify(existingData, null, 2));
+      
+      res.send('Datos guardados correctamente.');
+    } catch (error) {
+      res.status(400).send('Error al procesar datos.');
+    }
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
